0ca7472e0041ac6ba0c3eb171ebaf3c57ec6bab5
sukamat <sukamat@adobe.com>
2024-03-13 16:20:32 -0700

    Format and clean up Milo edgeworker

--- milo/Milo_floodgate_edge_workers/main.js
+++ milo/Milo_floodgate_edge_workers/main.js
@@ -1,82 +1,87 @@
-// comment
+/* ***********************************************************************
+ * ADOBE CONFIDENTIAL
+ * ___________________
+ *
+ * Copyright 2024 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of Adobe and its suppliers, if any. The intellectual
+ * and technical concepts contained herein are proprietary to Adobe
+ * and its suppliers and are protected by all applicable intellectual
+ * property laws, including trade secret and copyright laws.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe.
+ ************************************************************************* */
 
 import { httpRequest } from 'http-request';
-import { createResponse } from 'create-response';
-import {logger} from 'log'
+import { logger } from 'log';
 import { Cookies } from 'cookies';
 
 const PINK_ENV = 'pink';
-const CONFIG_URL = "/drafts/floodgate/configs/edge-worker-config.json";
+const CONFIG_URL = '/drafts/floodgate/configs/edge-worker-config.json';
 
 async function getConfig() {
-    var configReq = await httpRequest(CONFIG_URL); //http subrequest to get the body of the json file in Adobe's main server
-    var configObj = await configReq.json(); //read the body of the json file
+    // http subrequest to get the body of the json file in Adobe's main server
+    const configReq = await httpRequest(CONFIG_URL);
+    const configObj = await configReq.json();
     return configObj;
 }
 
 export async function onClientRequest(request) {
-    
-    let origin;
-    const config = await getConfig(); 
-    let eventStartTime = new Date(config.times.data[0].startTime)
-    let eventEndTime = new Date(config.times.data[0].endTime)
-    let eventPathRegex = new RegExp(config.paths.data[0].paths)
-
-    let path = request.path
-    
-    let isPathMatch = eventPathRegex.test(path)
+    const config = await getConfig();
+    const eventStartTime = new Date(config.times.data[0].startTime);
+    const eventEndTime = new Date(config.times.data[0].endTime);
+    const eventPathRegex = new RegExp(config.paths.data[0].paths);
+    const { path } = request;
+    const isPathMatch = eventPathRegex.test(path);
 
     if (!isPathMatch) {
-        logger.log("no matching event")
+        logger.log('no matching event');
         return;
     }
-    
-    let currDateTime = new Date()
-    
-    if (currDateTime > eventStartTime && currDateTime < eventEndTime)
-    
-    {
-        logger.log ("current time is after event start, going to PINK")
-        request.setVariable('PMUSER_EW_FG_VERSION','pink');
+
+    const currDateTime = new Date();
+
+    let origin;
+    if (currDateTime > eventStartTime && currDateTime < eventEndTime) {
+        logger.log('current time is after event start, going to PINK');
+        request.setVariable('PMUSER_EW_FG_VERSION', 'pink');
         origin = PINK_ENV;
-        
-    } else if (currDateTime > eventEndTime ) {
-        logger.log ("current time is after the event ends, going to MAIN server")
-        //do nothing - go to default server 
-    }
-    
-    else {
+    } else if (currDateTime > eventEndTime) {
+        logger.log('current time is after the event ends, going to MAIN server');
+        // do nothing - go to default server
+    } else {
         // Access the cookies from the request object
-        
-        //create a loop to validate all cookie headers executes the length of the array 
-        
+        // Create a loop to validate all cookie headers executes the length of the array
         const cookieHeader = request.getHeader('Cookie');
         const cookies = new Cookies(cookieHeader);
         const fgCookie = cookies.get('fg');
         const verifyRegex = request.getVariable('PMUSER_EW_FG_NTH_WORD');
-        const fgCookiePattern = new RegExp(verifyRegex)
+        const fgCookiePattern = new RegExp(verifyRegex);
 
-        //logger.log('Request cookies are:' + cookies), Check if the "fg" cookie is present
-        // iterate through the cookies and if it matches - apply the logic 
-        
-        logger.log('The cookie pattern is: ' + fgCookiePattern.test(fgCookie) + ', The cookie header is: [' + cookieHeader[0] + ']' + ', Extract fgCookie: [' + fgCookie + ']','Extract verifyRegex: [' + verifyRegex + ']')
-                
-        if (fgCookiePattern.test(fgCookie)) { //boolean - should be true or false
+        // logger.log('Request cookies are:' + cookies), Check if the "fg" cookie is present
+        // iterate through the cookies and if it matches - apply the logic
 
-                request.setVariable('PMUSER_EW_FG_VERSION','pink');
-                request.setHeader('X-Adobe-Floodgate', 'pink');                
-                logger.log("cookie matches: PINK origin");
+        logger.log(`The cookie pattern is: ${fgCookiePattern.test(fgCookie)}, 
+                    The cookie header is: [${cookieHeader[0]}], 
+                    Extract fgCookie: [${fgCookie}], 
+                    Extract verifyRegex: [${verifyRegex}]`);
 
-                origin = PINK_ENV
-                
-            }  else {
-
-                logger.log("cookie doesn't match: go to Main origin");
-                //do nothing - go to default server
-            }   
-        }
-        
-        if (origin) {
-            request.route({origin: origin});
+        if (fgCookiePattern.test(fgCookie)) {
+            // boolean - should be true or false
+            request.setVariable('PMUSER_EW_FG_VERSION', 'pink');
+            request.setHeader('X-Adobe-Floodgate', 'pink');
+            logger.log('cookie matches: PINK origin');
+            origin = PINK_ENV;
+        } else {
+            logger.log("cookie doesn't match: go to Main origin");
+            // do nothing - go to default server
         }
     }
+
+    if (origin) {
+        request.route({ origin });
+    }
+}
